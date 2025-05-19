4d2c1506f20c7118ba768957dd5cd27bbdbb1a88
Christian Boleku <158060444+proudhAteR@users.noreply.github.com>
2025-05-18 22:02:54 -0400

    Feat/profile page (#1)
    * git: updated the gitignore file
    
    * feat: added the user profile picture on the profile page
    
    * feat: finished the base UI for the user profile view
    
    * feat: made the frontend for the profile picture configuration
    
    * sarted working on the profile view backend functionnalities

--- public/javascripts/vault.js
+++ public/javascripts/vault.js
@@ -20,18 +20,23 @@ export function togglePassword() {
     });
 }
 
-export function focusOnManage() {
-    document.querySelectorAll('[data-manage]').forEach(btn => {
+export function focusOnModal() {
+    document.querySelectorAll('[data-manage], [data-actions]').forEach(btn => {
         btn.addEventListener('click', () => {
             const modalSelector = btn.getAttribute('data-bs-target');
             const modal = document.querySelector(modalSelector);
+
+            if (!modal) return;
+            modal.removeAttribute('aria-hidden');
 
             const instance = bootstrap.Modal.getOrCreateInstance(modal);
             instance.show();
 
             modal.addEventListener('shown.bs.modal', () => {
                 modal.querySelector('input')?.focus();
-            }, { once: true });
+            }, {once: true});
+
+            console.log('test')
         });
     });
 }
