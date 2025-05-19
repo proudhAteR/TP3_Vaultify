287b62efdcb39b40ddefd0fc8713612515254d26
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2025-05-18 21:58:06 -0400

    sarted working on the profile view backend functionnalities

--- app/Controllers/Profile/ProfileController.php
+++ app/Controllers/Profile/ProfileController.php
@@ -5,6 +5,7 @@ namespace Controllers\Profile;
 use Controllers\AppController;
 use Zephyrus\Network\Response;
 use Zephyrus\Network\Router\Get;
+use Zephyrus\Network\Router\Post;
 use Zephyrus\Network\Router\Root;
 
 #[Root('/profile')]
@@ -14,5 +15,12 @@ class ProfileController extends AppController
     public function index(): Response
     {
         return $this->display(page: "profile", args: ["title" => "Profile"]);
+    }
+
+    #[Post('/upload-avatar')]
+    public function uploadAvatar(): Response
+    {
+        $avatar = $this->request->getBody()->getParameter("avatar");
+        return $this->abortNotImplemented();
     }
 }
